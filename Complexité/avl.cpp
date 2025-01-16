276361c4d3f12332f1ed4436d7a93ffb3b5b262e
julienc <162288701+zulianc@users.noreply.github.com>
2025-01-16 09:57:50 +0100

    Add files via upload

---
+++ Complexité/avl.cpp
@@ -0,0 +1,102 @@
+/*
+Instructions :
+
+1 - Compléter le code ci-dessous pour avoir une implémentation
+d'un arbre de type AVL. 
+
+2 - Faire des tests d'insertion, de suppression et de recherche (penser à faire une fonction d’affichage). 
+Faire en sorte d’avoir toutes les possibilités dans vos tests.
+
+3 – Considérer le fichier "avl_bench.cpp". Il génère des ensembles de benchmarking avec des tailles d’arbre différentes : 
+de 2^5 éléments dans l’arbre à 2^25 éléments. Pour chaque taille, il donne 3 fichiers :
+Values, Search et Delete, contenant respectivement les valeurs à insérer dans l’arbre, 
+les valeurs à rechercher, et celles à supprimer. Ajouter dans ce fichier (avl.cpp) les codes nécessaire pour lire 
+les fichiers de benchmark et effectuer les opérations correspondantes.
+
+4 – Ajouter du code pour chronométrer les opérations. Réaliser des courbes comparatives 
+des temps d’exécutions des différentes opération en fonction de la taille de l’arbre. 
+Retrouvons-nous bien les complexité logarithmiques promises ?
+*/
+
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+// Définition du type noeud de l'arbre ...
+typedef struct node {
+    uint64_t data;
+    int height;
+    struct node *left;
+    struct node *right;
+} node;
+
+// Balance Factor BF
+int bf(node *n) {
+    int bf = 0;
+    // Calcul le BF au niveau du noeud n dans l'arbre
+    return bf;   
+}
+
+// Rotation LL
+node* llrotation(node *n) {
+    node *p;
+    node *tp;
+    p = n;
+    tp = p->left;
+
+    p->left = tp->right;
+    tp->right = p;
+
+    return tp; 
+}
+
+// Rotation RR
+node* rrrotation(node *n) {
+    node *p;
+    node *tp;
+    p = n;
+    tp = p->right;
+
+    p->right = tp->left;
+    tp->left = p;
+
+    return tp; 
+}
+
+// Rotation RL
+node * rlrotation(node *n) {
+    node *tp;
+    // effectue la rotation RL
+    // TODO
+    return tp; 
+}
+
+// Rotation LR
+node* lrrotation(node *n){
+    node *tp;
+    // effectue la rotation LR
+    // TODO
+    return tp; 
+}
+
+// Insertion
+node* insert(node *root, uint64_t data){
+    // insère la valeur data au bon endroit dans l'arbre enraciné en root ...
+    // TODO
+    return root;
+}
+
+// Suppresion
+node* deleteNode(node *root, uint64_t data){
+    // cherche la valeur data dans l'arbre enraciné en root puis la supprime ...    
+    // TODO
+    return root;
+}
+
+    
+int main(){
+    node * root = NULL;
+    // remplir l'arbre
+    // faire des tests
+    return 0;
+}
---
+++ Complexité/avl_bench.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_int_distribution<uint64_t> dis;
+
+    vector<int> exposants = {5,10,15,20,25};
+    for(auto e:exposants)
+    {
+        uint32_t count = 1 << e;
+        string str1 = "Values_" + to_string(e) + ".txt";
+        string str2 = "Search_" + to_string(e) + ".txt";
+        string str3 = "Delete_" + to_string(e) + ".txt";
+
+        ofstream f1(str1);
+        ofstream f2(str2);
+        ofstream f3(str3);
+        for (size_t i = 0; i < count; i++)
+        {
+            uint64_t v = dis(gen);
+            f1 << v << '\n'; 
+            switch (i % 10) //30% iront en search, 20% en delete et autre 10% aux deux.
+            { 
+                case 0 :
+                case 1 :
+                case 2 : f2 << v << '\n'; break;
+                case 3 : 
+                case 4 : f3 << v << '\n'; break;
+                case 5 : f2 << v << '\n'; f3 << v << '\n'; break;
+                default : break;
+            }
+        }
+        f1.close();
+        f2.close();
+        f3.close();
+    }
+    return 0;
+}
